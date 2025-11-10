165ee1c5a8f64522a95b7d919357e80ed637a1db
Eskai García <yosoyeskai@gmail.com>
2025-11-10 09:29:57 +0100

    feat: implemented conditional titles for upgrade buttons

--- src/components/Upgrade.tsx
+++ src/components/Upgrade.tsx
@@ -12,10 +12,22 @@ interface UpgradeProps {
 }
 
 export default function Upgrade({ src, title, price, count, hidden, notAffordable, onClick } : UpgradeProps) {
+  let titleContent : React.ReactNode = title;
+  if (title.includes('/')) {
+    const [title0, title1] = title.split('/');
+    titleContent = ( 
+      <>
+        <span className="title-count0">{title0}</span>
+        <span className="title-count1">{title1}</span> 
+      </>
+    )
+  } 
+
   return (
     <button
       className={
         "upgrade " +
+        (count >= 1 ? '' : 'new ') +
         (hidden ? "hidden " : "") +
         (notAffordable ? "not-affordable" : "")
       }
@@ -23,9 +35,9 @@ export default function Upgrade({ src, title, price, count, hidden, notAffordabl
     >
       <img src={src} />
       <div>
-        <p className="upgradeName">{title}</p>
+        <p className="upgradeName">{titleContent}</p>
         <p className="upgradePrice">{price}</p>
-        {count !== undefined && <span>{count}</span>}
+        {count !== undefined && <span className="count">{count}</span>}
       </div>
     </button>
   );
